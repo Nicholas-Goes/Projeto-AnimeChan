2c3bdbd5528e27223426b55b3ef530502c12fabd
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-03-01 12:29:24 -0800

    Update TopAnime List

--- src/components/TopAnimeList/TopAnimeList.tsx
+++ src/components/TopAnimeList/TopAnimeList.tsx
@@ -7,7 +7,7 @@ interface ResponseData{
   score: number;
   description: string;
   images: {
-    jpg: {
+    webp: {
       image_url: string;
     }
   }  
@@ -27,15 +27,19 @@ const TopAnimeList = () => {
   }, [Jikan])
   return (
     <div className='TopAnime'>
-      <section className='TopAnime--listarea'>
-        {TopList.map(TopList => {
-          return(
-            <div className='TopAnime--listitem'>
-              <img src={TopList.images.jpg.image_url} alt={TopList.description} />
-            </div>
+      <div className='TopAnime--listarea'>
+        <div className='TopAnime--list' style={{
+          width: TopList.length * 260,
+        }}>
+          {TopList.length > 0 && TopList.map(TopList => {
+            return(
+              <div className='TopAnime--listitem'>
+                <img src={TopList.images.webp.image_url} alt={TopList.description} />
+              </div>
           )
-        })}
-      </section>      
+          })}
+        </div>   
+      </div>   
     </div>
   )
 };
