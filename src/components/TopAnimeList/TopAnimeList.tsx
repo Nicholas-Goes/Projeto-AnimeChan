bdfe95172dbfbedd69d6dfd43a24acc1e5d147d1
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-03-01 11:47:07 -0800

    Update TopAnimeList

--- src/components/TopAnimeList/TopAnimeList.tsx
+++ src/components/TopAnimeList/TopAnimeList.tsx
@@ -1,20 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Jikan from '../../services/Jikan';
-
 import './TopAnimeList.css';
 
+interface ResponseData{
+  title: string;
+  score: number;
+  description: string;
+  images: {
+    jpg: {
+      image_url: string;
+    }
+  }  
+}
+
 const TopAnimeList = () => {
+
+  const [TopList, setTopList] = useState<ResponseData[]>([]);
 
   useEffect(()=> {
     Jikan
       .get(`top/anime`)
-      .then(response => console.log(response.data.data))
+      .then(response => {
+        setTopList(response.data.data)
+      })
       .catch(err => console.log(err));
-  }, [])
+  }, [Jikan])
   return (
     <div className='TopAnime'>
-      <section className='TopAnimeList--list'>
-
+      <section className='TopAnime--listarea'>
+        {TopList.map(TopList => {
+          return(
+            <div className='TopAnime--listitem'>
+              <img src={TopList.images.jpg.image_url} alt={TopList.description} />
+            </div>
+          )
+        })}
       </section>      
     </div>
   )
