c71e8884b39a96debf971f9ede9baf7efb8df77e
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-03-02 15:00:23 -0800

    Update Header and Styles

--- src/components/AnimeMenu/AnimeMenu.tsx
+++ src/components/AnimeMenu/AnimeMenu.tsx
@@ -1,16 +1,18 @@
-import "./AnimeMenu.css"
+import "./AnimeMenu.css";
 
 function AnimeMenu() {
   return (
     <nav className="AnimeMenu">
       <ul className="AnimeMenu--ul">
         <li className="AnimeMenu--li">
-          <img className="AnimeMenu--logo" src="src/assets/anime-icon.png" />
-          <a href="#" className="AnimeMenu--a">Anime<span>Chan</span></a>
+          <img className="AnimeMenu--logo" src="https://img.icons8.com/fluency/48/000000/water.png"/>
+          <a href="#" className="AnimeMenu--a">Anime<span>Element</span></a>
         </li>
 
         <li className="AnimeMenu--li">
-          <a href="#" className="AnimeMenu--a">Menu</a>
+          <a href="#" className="AnimeMenu--a">
+            <img src="https://img.icons8.com/ios-glyphs/30/ffffff/menu--v1.png"/>
+          </a>
         </li>
       </ul>
     </nav>
