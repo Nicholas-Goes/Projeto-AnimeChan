b9822f7a7d24ca151d95e555f50d4bf7cfec980a
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-03-05 13:30:46 -0800

    Sleep request

--- src/service/Jikan.ts
+++ src/service/Jikan.ts
@@ -1,8 +1,24 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 const Jikan = axios.create({
   baseURL: 'https://api.jikan.moe/v4',
-  timeout: 25000,
+});
+
+const sleepRequest = (milliseconds: number, originalRequest: AxiosRequestConfig<any>) => {
+  return new Promise((resolve) => {
+      setTimeout(() => resolve(Jikan(originalRequest)), milliseconds);
+  });
+};
+
+Jikan.interceptors.response.use(response => {
+  return response;
+}, error => {
+  const { config, response: { status }} = error;
+  const originalRequest = config;
+
+  if (status === 429) {
+      return sleepRequest(1000, originalRequest);
+  }
 });
 
 export default Jikan;
