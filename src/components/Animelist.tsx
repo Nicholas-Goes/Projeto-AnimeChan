98330837c26d94bfcf2cddd82ed08c720b3f5fcb
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-05-02 07:40:50 -0300

    Adding Rate limiter to avoid blank spaces

---
+++ src/components/Animelist.tsx
@@ -0,0 +1,5 @@
+function AnimeList() {
+
+}
+
+export default AnimeList;
--- src/services/Jikan.ts
+++ src/services/Jikan.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
+import rateLimiter from 'axios-rate-limit'
 
-const Jikan = axios.create({
-    baseURL: `https://api.jikan.moe/v4`,
-});
+const BASE_URL = 'https://api.jikan.moe/v4';
+
+const Jikan = rateLimiter(axios.create({
+    baseURL: BASE_URL
+}), {maxRequests: 3, perMilliseconds: 1000, maxRPS: 3})
 
 export default Jikan;
