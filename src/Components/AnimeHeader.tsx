4b667da85b08967251e4462aa029df62879862e3
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-05-10 09:31:59 -0300

    Update Header

--- src/Components/AnimeHeader.tsx
+++ src/Components/AnimeHeader.tsx
@@ -3,7 +3,10 @@ import './AnimeHeader.css'
 function AnimeHeader() {
     return(
         <header>
-            This is a Header
+            <section className='content'>
+                <h1>Hello World!!! I'm alive</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, eos et hic sed ipsa culpa id quos laborum suscipit quidem. Incidunt blanditiis quos laboriosam ex rem modi corrupti possimus ipsa!</p>
+            </section>
         </header>
     )
     
