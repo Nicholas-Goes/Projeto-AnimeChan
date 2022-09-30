b1e32c64d4570fed411eb6efe1d53e14f04fc18b
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-09-30 10:48:54 -0300

    Update Content section

--- src/Components/AnimeContent.tsx
+++ src/Components/AnimeContent.tsx
@@ -4,11 +4,10 @@ function AnimeContent() {
   return (
     <section className="content">
       <h1>Subscribe to Newslatter</h1>
-      <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-      <label htmlFor="password">Email</label>
-        <input type="password" name="password" id="password" />
-
+        <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
+        <label htmlFor="password">Email</label>
+          <input type="password" name="password" id="password" />
     </section>
   )
 }
